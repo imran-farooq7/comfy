10b8115883f9589748c797e5e97b82e015cfe9d0
Imran Farooq <imran_farooq7@hotmail.com>
2024-06-21 02:14:10 +0500

    Router setup is complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,64 @@
-export default function App() {
-	return <button className="btn">Home</button>;
-}
+import {
+	Login,
+	About,
+	Cart,
+	Checkout,
+	Error,
+	HomeLayout,
+	Landing,
+	Orders,
+	Product,
+	Products,
+	Register,
+} from "./pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+			{
+				path: "orders",
+				element: <Orders />,
+			},
+			{
+				path: "products",
+				element: <Products />,
+			},
+			{
+				path: "product/:id",
+				element: <Product />,
+			},
+			{
+				path: "checkout",
+				element: <Checkout />,
+			},
+		],
+	},
+	{
+		path: "login",
+		element: <Login />,
+	},
+	{
+		path: "register",
+		element: <Register />,
+	},
+]);
+const App = () => {
+	return <RouterProvider router={router} />;
+};
+
+export default App;
