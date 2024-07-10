281f4d0f72072e14ff163b0ac3934cbd62d667b8
Imran Farooq <imran_farooq7@hotmail.com>
2024-07-11 03:39:56 +0500

    Featured products component completed

---
+++ src/components/ProductsGrid.tsx
@@ -0,0 +1,47 @@
+import { Link, useLoaderData } from "react-router-dom";
+interface Product {
+	id: number;
+	attributes: {
+		category: string;
+		colors: string[];
+		company: string;
+		description: string;
+		featured: boolean;
+		image: string;
+		price: string;
+		shipping: boolean;
+		title: string;
+	};
+}
+const ProductsGrid = () => {
+	const products = useLoaderData() as Product[];
+
+	return (
+		<div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+			{products.map((product) => {
+				const { title, price, image } = product.attributes;
+				const dollarsAmount = price;
+				return (
+					<Link
+						key={product.id}
+						to={`/products/${product.id}`}
+						className="card w-full  shadow-xl hover:shadow-2xl transition duration-300 "
+					>
+						<figure className="px-4 pt-4">
+							<img
+								src={image}
+								alt={title}
+								className="rounded-xl h-64 md:h-48 w-full object-cover"
+							/>
+						</figure>
+						<div className="card-body items-center text-center">
+							<h2 className="card-title capitalize tracking-wider">{title}</h2>
+							<span className="text-secondary">{dollarsAmount}</span>
+						</div>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
+export default ProductsGrid;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,16 @@
 import { Hero } from "../components";
 import Axios from "../../utils/axios";
+import FeaturedProducts from "../components/FeaturedProducts";
 export const loader = async () => {
 	const products = await Axios.get("products?featured=true");
-	return products.data;
+	return products.data.data;
 };
 
 const Landing = () => {
 	return (
 		<div>
 			<Hero />
+			<FeaturedProducts />
 		</div>
 	);
 };
